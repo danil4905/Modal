b3842670ea8daf3c6b5c1b01ddba5462e265f382
Danil Konovalov <47856599+danil4905@users.noreply.github.com>
2021-04-24 11:49:47 +0400

    Обернул в функцию onload

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-let count = 1,
+window.onload = function () {
+  let count = 1,
     sum = 0,
     min = 1,
     max = 5,
     totalPrice = 0,
     optionSum = 0,
     flatWhitePrice = 125;
-let btnPlus = document.getElementById("pluse-btn"),
+  let btnPlus = document.getElementById("pluse-btn"),
     btnMinus = document.getElementById("minus-btn"),
     counter = document.getElementById("count"),
     coffePrice = document.getElementById("coffe-price"),
     prices = document.getElementsByClassName("option-item"),
-    cartCounter = document.getElementById('cart-add'),
-    event = document.getElementsByTagName('section'),
-    totalResult = document.getElementById('cart-total');
+    cartCounter = document.getElementById("cart-add"),
+    event = document.getElementsByTagName("section"),
+    totalResult = document.getElementById("cart-total");
 
-let renderResults = (place, result) => {
-  place.innerHTML = result;
-};
-let renderTotalPrice = () => {
-    totalPrice = count*flatWhitePrice + findOptionSum();
-    renderResults(totalResult,`$ ${totalPrice}`);
-}
-renderResults(counter, count);
-renderResults(cartCounter,`ADD ${count} TO CART`)
-renderResults(coffePrice, `$  ${count * flatWhitePrice}`);
+  let renderResults = (place, result) => {
+    place.innerHTML = result;
+  };
+  let renderTotalPrice = () => {
+    totalPrice = count * (flatWhitePrice + findOptionSum());
+    renderResults(totalResult, `$ ${totalPrice}`);
+  };
+  renderResults(counter, count);
+  renderResults(cartCounter, `ADD ${count} TO CART`);
+  renderResults(coffePrice, `$  ${count * flatWhitePrice}`);
 
-let findOptionSum = () => {
-  optionSum = 0;
-  for (let i = 0; i < prices.length; i++) {
-  if (prices[i].checked) {
-    optionSum += prices[i].value*1;
-  }
-}
-return optionSum;
-}
+  let findOptionSum = () => {
+    optionSum = 0;
+    for (let i = 0; i < prices.length; i++) {
+      if (prices[i].checked) {
+        optionSum += prices[i].value * 1;
+      }
+    }
+    return optionSum;
+  };
 
-renderTotalPrice();
-
-btnMinus.onclick = function () {
-  if (count > min) {
-    count = count - 1;
-    renderResults(counter, count);
-    renderResults(cartCounter,`ADD ${count} TO CART`)
-    renderResults(coffePrice, '$ ' + count * flatWhitePrice);
-    renderTotalPrice();
-  }
-};
-btnPlus.onclick = function () {
-  if (count < max) {
-    count = count + 1;
-    renderResults(counter, count);
-    renderResults(cartCounter,`ADD ${count} TO CART`)
-    renderResults(coffePrice, '$ ' + count * flatWhitePrice);
-    renderTotalPrice();
-  }
-};
-for(let i = 0;event.length;i++) {
-  event[i].onclick = function () {
   renderTotalPrice();
-}
-}
+
+  btnMinus.onclick = function () {
+    if (count > min) {
+      count = count - 1;
+      renderResults(counter, count);
+      renderResults(cartCounter, `ADD ${count} TO CART`);
+      renderResults(coffePrice, "$ " + count * flatWhitePrice);
+      renderTotalPrice();
+    }
+  };
+  btnPlus.onclick = function () {
+    if (count < max) {
+      count = count + 1;
+      renderResults(counter, count);
+      renderResults(cartCounter, `ADD ${count} TO CART`);
+      renderResults(coffePrice, "$ " + count * flatWhitePrice);
+      renderTotalPrice();
+    }
+  };
+  for (let i = 0; event.length; i++) {
+    event[i].onclick = function () {
+      renderTotalPrice();
+    };
+  }
+};
